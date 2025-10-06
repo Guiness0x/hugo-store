750c353bd291fc17c624de71bae7b075984240ae
Guiness <kilo.drop@proton.me>
2025-10-06 11:54:54 -0400

    Force custom Hugo build with manual env

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
 
-# Force latest Hugo version
 HUGO_VERSION=0.123.7
 
-echo "Installing Hugo v$HUGO_VERSION..."
-wget -qO- https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz | tar -xz
+echo ">>> Installing Hugo v${HUGO_VERSION}..."
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo.tar.gz
+chmod +x ./hugo
 
-echo "Running Hugo build..."
-./hugo --minify
+echo ">>> Building site..."
+./hugo --gc --minify --cleanDestinationDir
